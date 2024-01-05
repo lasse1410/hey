14aa6e822076234e5d6226331f75c6785df129b8
lasse1410 <60522034+lasse1410@users.noreply.github.com>
2024-01-05 19:24:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 3001;
+const port = 3004;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
